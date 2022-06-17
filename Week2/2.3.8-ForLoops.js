82fec17db031d25edec4b6968c50d9ae1806994d
Angie DeCuir <adecuir8@gmail.com>
2022-06-16 21:42:23 -0500

    Week2

---
+++ Week2/2.3.8-ForLoops.js
@@ -0,0 +1,16 @@
+// Create a for loop that TOTALS the numbers from 1 to 100
+// eslint-disable-next-line no-unused-vars
+const num = 100;
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+  sum += i;
+  console.log(sum);
+}
+const num = 100;
+let sum = 0;
+
+for (let i = 1; i <= 100; i--) {
+  sum += i;
+  console.log(sum);
+}
